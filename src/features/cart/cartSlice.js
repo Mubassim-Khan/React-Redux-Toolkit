b6002efbcb5f625a711eaee95cef81e788a8bce5
Mubassim Ahmed Khan <mubassimkhan@gmail.com>
2024-01-23 21:31:46 +0500

    Added createAsyncThunk function, to pass the API url, & handle errors.

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,12 +1,30 @@
-import { createSlice } from "@reduxjs/toolkit";
-import cartItems from "../../cartItems"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const url = "https://course-api.com/react-useReducer-cart-project";
 
 const initialState = {
-    cartItems: cartItems,
+    cartItems: [],
     numOfItems: 4,
     total: 0,
     isLoading: true
 }
+
+/* Imp Points for createAsyncThunk()
+- To get the state (or other features of app) we can pass "thunkAPI" parameter to the function.
+- Can even use dispatch fuction using thunkAPI.
+- To pass the value into other components use paramter 'name'.
+*/
+
+export const getCartItems = createAsyncThunk('cart/getCartItems', async (thunkAPI) => {
+    try {
+        const resp = await axios(url);
+        return resp.data;
+    } catch (err) {
+        console.error(err);
+        return thunkAPI.rejectWithValue(err.response);
+    }
+})
 
 const cartSlice = createSlice({
     name: "cart",
@@ -33,10 +51,21 @@ const cartSlice = createSlice({
             state.cartItems.forEach((item) => {
                 amount += item.amount;
                 total += item.amount * item.price;
-            }) 
+            })
             state.numOfItems = amount;
             state.total = total;
-            
+        }
+    },
+    extraReducers: {
+        [getCartItems.fulfilled]: (state, action) => {
+            state.isLoading = false
+            state.cartItems = action.payload
+        },
+        [getCartItems.pending]: (state) => {
+            state.isLoading = true
+        },
+        [getCartItems.rejected]: (state) => {
+            state.isLoading = false
         }
     }
 })
